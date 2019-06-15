54e8f7564690fbe49636e410c59c162317f8c86a
kokoa <kokoapooh@gmail.com>
2019-06-15 14:10:42 +0900

    add interactive chat function

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,18 +1,47 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        #join room
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        #leave room
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
+    # receive message from websocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        #send message
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message': message
+            }
+        )
+        
+    # recerve message from room
+    def chat_message(self,event):
+        message = event['message']
+
+    # send message to websocket
         self.send(text_data=json.dumps({
             'message':message
         }))
